f49448edcd1a9caae587736e4133fd91e0ad157f
damielcowen <dbcohen7260@gmail.com>
2021-04-18 21:19:36 -0600

    updated WA

--- extractWashington.py
+++ extractWashington.py
@@ -2,24 +2,44 @@ import json
 from selenium import webdriver
 from datetime import datetime
 
-path_to_chromeDriver = '/Users/drahcir1/Documents/chromedriver'
-url ='https://www.walottery.com/Scratch/'
 
-# disables chrome from loading images 
-# https://stackoverflow.com/questions/28070315/python-disable-images-in-selenium-google-chromedriver
-chrome_options = webdriver.ChromeOptions()
-prefs = {"profile.managed_default_content_settings.images": 2}
-chrome_options.add_experimental_option("prefs", prefs)
-
-#creates a selenium chrome driver
-driver = webdriver.Chrome(path_to_chromeDriver,options=chrome_options)
-driver.implicitly_wait(5)
-driver.get(url)
-games_data = driver.execute_script('return WaLottery.Scratch.data')
-driver.close()
-
-today = datetime.today().strftime('%Y%m%d')
-with open (f"data/washington/{today}_WAscratchers.json", "w") as file:
-    json.dump(games_data, file)
+class scrapeScratchGames:
     
-    
+    def __init__(self):
+        self.url ='https://www.walottery.com/Scratch/'
+        self.path_to_chromeDriver = '/Users/drahcir1/Documents/chromedriver'
+        
+    def chrome_driver(self):
+        """
+        creates a selenium chrome web driver instance with select options 
+        -disables chrome from loading images 
+            -https://stackoverflow.com/questions/28070315/python-disable-images-in-selenium-google-chromedriver
+        """
+        self.chrome_options = webdriver.ChromeOptions()
+        prefs = {"profile.managed_default_content_settings.images": 2}
+        self.chrome_options.add_experimental_option("prefs", prefs)
+        self.driver = webdriver.Chrome(self.path_to_chromeDriver,options=self.chrome_options)
+        self.driver.implicitly_wait(5)
+        
+    def get_games_data(self):
+        '''
+        Washington lotto has a convinent json dictonary that can be accessed with the single line below.
+        Thank you Bye and Bye State
+        
+        retrives the varialbe and closes the driver
+        
+        ''' 
+        self.driver.get(self.url)
+        self.games_data = self.driver.execute_script('return WaLottery.Scratch.data')
+        self.driver.close()
+        
+    def save_json(self):
+
+        today = datetime.today().strftime('%Y%m%d')
+        with open (f"data/washington/{today}_WAscratchers.json", "w") as file:
+            json.dump(self.games_data, file)
+            
+    def extract_Washington(self):
+        self.chrome_driver()
+        self.get_games_data()
+        self.save_json()
